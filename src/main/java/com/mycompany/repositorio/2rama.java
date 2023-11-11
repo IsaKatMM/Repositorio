b9fdef47d97d3d81ed28c80d009fe85285bfd0f6
IsaKatMM <148238830+IsabelMorocho07@users.noreply.github.com>
2023-11-11 18:54:36 -0500

    tercer commit de la rama segunda

--- src/main/java/com/mycompany/repositorio/2rama.java
+++ src/main/java/com/mycompany/repositorio/2rama.java
@@ -3,5 +3,6 @@ package com.mycompany.repositorio;
 public class 2rama {
     System.out.println("segunda rama " );
     System.out.println("cambio segunda rama " );
+    System.out.println("fusionar con la rama develop " );
     
 }
