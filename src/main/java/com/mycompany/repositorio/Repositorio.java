6269ab84007dc8c1ac453c72092d4ee33272056b
IsaKatMM <148238830+IsabelMorocho07@users.noreply.github.com>
2023-11-11 13:09:35 -0500

    mi proyecto de github

---
+++ src/main/java/com/mycompany/repositorio/Repositorio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorio;
+
+/**
+ *
+ * @author HP
+ */
+public class Repositorio {
+
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 5;
+        
+        int suma = numero1 + numero2;
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+         System.out.println("Hola mundo este es mi repositorio :3 " );
+         System.out.println("By: Isa " );
+    }
+}
