1c3858450fddd5f81d75c6124e4dfc97ade49c15
IsaKatMM <148238830+IsabelMorocho07@users.noreply.github.com>
2023-11-11 18:34:17 -0500

    primer commit

--- src/main/java/com/mycompany/repositorio/Repositorio.java
+++ src/main/java/com/mycompany/repositorio/Repositorio.java
@@ -18,5 +18,6 @@ public class Repositorio {
         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
          System.out.println("Hola mundo este es mi repositorio :3 " );
          System.out.println("By: Isa " );
+         System.out.println("develop encargado de recibir todo el desarrlllo del proyecto " );
     }
 }
