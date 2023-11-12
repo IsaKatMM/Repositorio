6c37fa70a61b858a624139ec2dbb103166223077
IsaKatMM <148238830+IsabelMorocho07@users.noreply.github.com>
2023-11-11 19:06:57 -0500

    commit a incorporar cambios en develop

--- src/main/java/com/mycompany/repositorio/primerrama.java
+++ src/main/java/com/mycompany/repositorio/primerrama.java
@@ -2,5 +2,5 @@ package com.mycompany.repositorio;
 
 public class primerrama {
     System.out.println("primer rama " );
-    
+    System.out.println("cambios a incorporarse en la rama develop para nuevo funcionamiento" );
 }
